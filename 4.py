a57c288e709f6b91485ec0054cc041f44c10ec71
Bartosz Bryniarski <brylka@jelenia.com>
2026-05-09 11:29:41 +0200

    Sobota

--- 4.py
+++ 4.py
@@ -5,7 +5,10 @@ load_dotenv()
 # The client gets the API key from the environment variable `GEMINI_API_KEY`.
 client = genai.Client()
 
-response = client.models.generate_content(
-    model="gemini-3-flash-preview", contents="Cześć! Nazywam się Bartosz Bryniarski."
-)
-print(response.text)
+while True:
+    prompt = input("Prompt: ")
+
+    response = client.models.generate_content(
+        model="gemini-3-flash-preview", contents=prompt
+    )
+    print(f"GEMINI: {response.text}")
