e640c30cb01115dccfdb97054ec0fd9679f8f2a8
Bartosz Bryniarski <brylka@jelenia.com>
2026-05-09 13:25:32 +0200

    Sobota

--- 5.py
+++ 5.py
@@ -1,15 +1,18 @@
 from flask import Flask, render_template, request
 from google import genai
 from dotenv import load_dotenv
+import json
+
 load_dotenv()
 
 app = Flask(__name__)
 client = genai.Client()
-history = []
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    history = []
     if request.method == 'POST':
+        history = json.loads(request.form.get('history', '[]'))
         prompt = request.form.get('prompt', '')
         history.append({"role": "user", "parts": [{"text": prompt}]})
 
@@ -19,7 +22,7 @@ def index():
         history.append({"role": "model", "parts": [{"text": response.text}]})
         print(f"GEMINI: {response.text}")
 
-    return render_template("index.html", history=history)
+    return render_template("index.html", history=history, history_json=json.dumps(history))
 
 if __name__ == '__main__':
     app.run(debug=True)
