eb740f44f814d19a6f1f40ee4b39212fbcc99a0a
Bartosz Bryniarski <brylka@jelenia.com>
2026-05-09 12:25:20 +0200

    Sobota

--- 4.py
+++ 4.py
@@ -3,11 +3,14 @@ from dotenv import load_dotenv
 load_dotenv()
 
 client = genai.Client()
+history = []
 
 while True:
     prompt = input("Prompt: ")
+    history.append({"role": "user", "parts": [{"text": prompt}]})
 
     response = client.models.generate_content(
-        model="gemini-3-flash-preview", contents=prompt
+        model="gemini-3-flash-preview", contents=history
     )
+    history.append({"role": "model", "parts": [{"text": response.text}]})
     print(f"GEMINI: {response.text}")
