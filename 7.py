275b2971772a1c0cbf071e36f6ef4b9ac3b1117b
Bartosz Bryniarski <brylka@jelenia.com>
2026-05-10 09:53:41 +0200

    Niedziela

--- 7.py
+++ 7.py
@@ -12,27 +12,28 @@ LANGUAGE_ENDPOINT = os.getenv('AZURE_LANGUAGE_ENDPOINT')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    text = "Uwielbiam ten produkt! Działa świetnie i obsługa klienta jest fantastyczna."
+    if request.method == 'POST':
+        text = "Uwielbiam ten produkt! Działa świetnie i obsługa klienta jest fantastyczna."
 
-    # Konfiguracja klienta Azure
-    client = TextAnalyticsClient(
-        endpoint=LANGUAGE_ENDPOINT,
-        credential=AzureKeyCredential(LANGUAGE_KEY)
-    )
+        # Konfiguracja klienta Azure
+        client = TextAnalyticsClient(
+            endpoint=LANGUAGE_ENDPOINT,
+            credential=AzureKeyCredential(LANGUAGE_KEY)
+        )
 
-    # Analiza tonacji
-    documents = [text]
-    result = client.analyze_sentiment(documents=documents)[0]
+        # Analiza tonacji
+        documents = [text]
+        result = client.analyze_sentiment(documents=documents)[0]
 
-    # Sprawdź wynik
-    if not result.is_error:
-        print(f"Sentyment ogólny: {result.sentiment}")
-        print(f"  Pozytywny: {result.confidence_scores.positive:.2f}")
-        print(f"  Neutralny: {result.confidence_scores.neutral:.2f}")
-        print(f"  Negatywny: {result.confidence_scores.negative:.2f}")
-    else:
-        print(f"Błąd: {result.error.message}")
-    return "ok"
+        # Sprawdź wynik
+        if not result.is_error:
+            print(f"Sentyment ogólny: {result.sentiment}")
+            print(f"  Pozytywny: {result.confidence_scores.positive:.2f}")
+            print(f"  Neutralny: {result.confidence_scores.neutral:.2f}")
+            print(f"  Negatywny: {result.confidence_scores.negative:.2f}")
+        else:
+            print(f"Błąd: {result.error.message}")
+    return render_template("analiza.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
